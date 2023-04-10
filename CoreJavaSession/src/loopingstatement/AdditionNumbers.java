368ab8d609be041e39556340e97649d12207372b
91930 <91930@Dhanashri>
2023-04-10 11:51:59 +0530

    first

--- CoreJavaSession/src/loopingstatement/AdditionNumbers.java
+++ CoreJavaSession/src/loopingstatement/AdditionNumbers.java
@@ -5,24 +5,24 @@ public class AdditionNumbers {
 
 	void addNum() {
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter start point:");
+		System.out.println("Enter start point:"); //1
 		int s=sc.nextInt();
-		System.out.println("Enter the end point:");
+		System.out.println("Enter the end point:"); //3
 		int e=sc.nextInt();
 		int add=0;
 		while(s<=e)
 		{
-			add=add+s;
+			add=add+s; // 1+2+3 =6
 			s++;
 		}
 		System.out.println("Addition is:"+add);
 		sc.close();
 		}
+
 		
 		public static void main(String[] args) {
 			AdditionNumbers obj=new AdditionNumbers();
 			obj.addNum();
-
 		
 
 	}
