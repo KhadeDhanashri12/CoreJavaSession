8c5fa2903b7a07f111518ca09ea3385162360f5e
91930 <91930@Dhanashri>
2023-04-18 12:54:28 +0530

    first

--- CoreJavaSession/src/assignmentsonloop/Palindrome.java
+++ CoreJavaSession/src/assignmentsonloop/Palindrome.java
@@ -5,7 +5,7 @@ package assignmentsonloop;
 public class Palindrome {
 	public static void main(String args[]){  
 		  int r,sum=0,temp;    
-		  int n=1223;//It is the number variable to be checked for palindrome  
+		  int n=1221;//It is the number variable to be checked for palindrome  
 		  
 		  temp=n;    
 		  while(n>0){    
